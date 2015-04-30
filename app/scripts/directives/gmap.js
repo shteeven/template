b9d6f557c40260d484d94b0b81f89c135362d310
Steven Barnhurst <stevenbarnhurst@gmail.com>
2015-04-30 09:39:44 +0900

    finish gmap directive

--- app/scripts/directives/gmap.js
+++ app/scripts/directives/gmap.js
@@ -3,69 +3,25 @@ var app = angular.module('templateApp');
 app.directive('swbGmap', ['DataFactory', function (DataFactory) {
 
   return {
-    scope:{ swbUrl: '=' },
+    scope:{ swbUrl: '@' },
     restrict: 'E',
     template: '<div></div>',
     replace: true,
 
     link: function($scope, attrs, elem) {
 
-      $scope.mapData = {};
       DataFactory.getData($scope.swbUrl).success(function(data) {
-
-        console.log(data);
-
         var myLatLng = new google.maps.LatLng(data.map.lat, data.map.lng);
         //set option vars
-        var options = { center: myLatLng, zoom: data.map.zoom };
+        var options = { center: myLatLng, zoom: data.map.zoom, scrollwheel: data.map.scrollwheel };
         //initialize map
         var map = new google.maps.Map(document.getElementById("map-canvas"), options);
         setMarkers(map, data.sites);
-
-
       }).error(function(err) {console.log(err)});
-
-
-
 
       //initialize info window
       var infowindow = null;
       infowindow = new google.maps.InfoWindow();
-      // Make position for center map
-      //var myLatLng = new google.maps.LatLng(39.58108, -105.63535);
-      //set option vars
-      //var options = { center: myLatLng, zoom: 4 };
-      //initialize map
-      //var map = new google.maps.Map(document.getElementById("map-canvas"), options);
-      //create map markers
-      /*var sites = [
-        ['Mount Evans', $scope.swbUrl, -105.63535, 4, 'This is Mount Evans.'],
-        ['Irving Homestead', 40.315939, -105.440630, 2, 'This is the Irving Homestead.'],
-        ['Badlands National Park', 43.785890, -101.90175, 1, 'This is Badlands National Park'],
-        ['Flatirons in the Spring', 39.99948, -105.28370, 3, 'These are the Flatirons in the spring.']
-      ];*/
-      var iconA = 'images/icons/map/red/red_MarkerA.png';
-
-      /*function setMarkers(map, markers) {
-        for (var i = 0; i < markers.length; i++) {
-          var sites = markers[i];
-          var siteLatLng = new google.maps.LatLng(sites[1], sites[2]);
-          var marker = new google.maps.Marker({
-            position: siteLatLng,
-            map: map,
-            icon: iconA,
-            title: sites[0],
-            zIndex: sites[3],
-            html: sites[4]
-          });
-
-          google.maps.event.addListener(marker, "click", function () {
-            infowindow.setContent(this.html);
-            infowindow.open(map, this);
-          });
-        }
-      }*/
-      //setMarkers(map, sites);
 
       function setMarkers(map, markers) {
         for (var i = 0; i < markers.length; i++) {
@@ -74,19 +30,17 @@ app.directive('swbGmap', ['DataFactory', function (DataFactory) {
           var marker = new google.maps.Marker({
             position: siteLatLng,
             map: map,
-            icon: iconA,
+            icon: site.icon,
             title: site.title,
             zIndex: site.zIndex,
             html: site.html
           });
-
           google.maps.event.addListener(marker, "click", function () {
             infowindow.setContent(this.html);
             infowindow.open(map, this);
           });
         }
       }
-
     }
   };
 }]);
---
+++ app/scripts/directives/mailchimp.js
@@ -0,0 +1,44 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name templateApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the templateApp
+ */
+
+var app = angular.module('templateApp');
+
+app.directive('swbCarousel', ['$interval', 'DataFactory', function ($interval, DataFactory) {
+  return {
+    scope:{ swbUrl: '@' },
+    restrict: 'EA',
+    templateUrl: 'views/templates/carousel.html',
+    link: function($scope, attrs, elem) {
+
+      // initialize active item
+      $scope.itemSelect = function(item) {$scope.item = item};
+      $scope.next = function() {
+        if ($scope.item === $scope.indexes) {
+          $scope.item = 0;
+        } else {
+          $scope.item = $scope.item+1;
+        }
+      };
+      $scope.prev = function() {
+        if ($scope.item === 0) {
+          $scope.item = $scope.indexes;
+        } else {
+          $scope.item = $scope.item-1;
+        }
+      };
+
+      DataFactory.getData($scope.swbUrl).success(function(data) {
+        $scope.images = data;
+        $scope.indexes = data.length-1;
+        $scope.item = 0;
+      }).error(function(err) {console.log(err)});
+    }
+  };
+}]);
